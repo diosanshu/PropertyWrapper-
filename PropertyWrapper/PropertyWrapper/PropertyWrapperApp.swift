6090acf6d391d187d5b35807ad46b935dd798078
diosanshu <diosanshu@gmail.com>
2023-12-28 10:23:20 +0530

    IDE Xcode added

---
+++ PropertyWrapper/PropertyWrapper/PropertyWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrapperApp.swift
+//  PropertyWrapper
+//
+//  Created by Haadhya on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
